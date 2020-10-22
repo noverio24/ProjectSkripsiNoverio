a1d144ea2195c3b795df6a1b797d660f9b69ad1e
Dony Rinaldo Radjah <donyradjah@gmail.com>
2020-10-22 20:52:01 +0700

    fix tampil user

--- app/src/main/java/com/belajar/projectskripsi/MainActivity.java
+++ app/src/main/java/com/belajar/projectskripsi/MainActivity.java
@@ -3,12 +3,22 @@ package com.belajar.projectskripsi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtNama = findViewById(R.id.txtNama);
+
+        FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        txtNama.setText(fAuth.getCurrentUser().getEmail());
     }
 }
